52792745994025e9f3100691a095059ed102e498
Hugh Nowlan <nosmo@netsoc.tcd.ie>
2014-02-26 16:48:28 +0000

    Major reorganisation of repo

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "botbanger",
+    version = "1.0.0",
+    author = "Vmon",
+    author_email = "vmon@equalit.ie",
+    description = "Model-based bot-banner",
+    license = "GNU Affero",
+    keywords = "banjax botnetdbp learn2ban",
+    url = "http://github.com/equalitie/botbanger",
+    packages=['botbanger'],
+    package_dir={'':'src'},
+    #long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Daemons",
+        "License :: GNU Affero License",
+        ],
+    scripts = ["src/logfetcher.py"],
+    data_files=[
+        ("/etc/botbanger", ["conf/botbanger.conf"])
+        ],
+    )
